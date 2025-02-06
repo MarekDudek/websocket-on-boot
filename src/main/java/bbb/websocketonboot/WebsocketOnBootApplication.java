a819176697e18cfadad3a2e30d83b742197d7fb8
Marek Dudek <marek.dudek@gmail.com>
2025-02-06 22:33:13 +0100

    fresh project

---
+++ src/main/java/bbb/websocketonboot/WebsocketOnBootApplication.java
@@ -0,0 +1,13 @@
+package bbb.websocketonboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsocketOnBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebsocketOnBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/bbb/websocketonboot/WebsocketOnBootApplicationTests.java
@@ -0,0 +1,13 @@
+package bbb.websocketonboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsocketOnBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
